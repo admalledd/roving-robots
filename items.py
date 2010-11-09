411408a3c0229475f2ad27c8b6e10da48a2d411f
gamesoda <randytec@gmail.com>
2010-11-08 16:47:14 -0800

    Added datalog item

--- items.py
+++ items.py
@@ -25,3 +25,8 @@ class metal(item):
     def __init__(self):
         self.surf = lib.common.load_img(os.path.join('items','scrap-metal.png'))
         self.value = random.randint(0,100)
+
+class datalog(item):
+    def __init__(self):
+        self.surf = pygame.Surface ((50,50))
+        self.value = 'test item!'

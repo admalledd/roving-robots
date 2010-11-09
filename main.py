24d66671c44252a3ae361b5f6f00e2888efffb20
Eric Driggers <admalledd@gmail.com>
2010-11-08 17:01:09 -0800

    example command line stuff for randy to use

--- main.py
+++ main.py
@@ -49,6 +49,8 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         #eventually change to be based off the optparse module instead...
 		##randy:: ill work on that!
+        
+        usage='''<script.py> -v$(NUMBER = 0) -l[--log-file] $(LOG_FILE_NAME = roving-robots.log)'''
         if sys.argv[1].startswith('-v'):
             lib.common.debug=len(sys.argv[1][1:])
     if lib.common.debug>0:logger.info('debug level is:%s'%lib.common.debug)
